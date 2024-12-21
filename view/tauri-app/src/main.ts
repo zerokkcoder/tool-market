6325ae28ba60bbc187cd4fb11f2404a59e7b1e61
zerokk <918031078@qq,com>
2024-12-21 17:26:35 +0800

    use tailwind & primevue

--- view/tauri-app/src/main.ts
+++ view/tauri-app/src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from "vue";
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 import App from "./App.vue";
+import "./assets/css/style.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+app.mount("#app");
--- view/tauri-app/vite.config.ts
+++ view/tauri-app/vite.config.ts
@@ -1,13 +1,25 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+import { fileURLToPath, URL } from 'node:url'
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [vue()],
-
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    }
+  },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
